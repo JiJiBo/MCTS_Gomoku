3f5a75f09f4238b70439e88a832f5f68d39e3879
NasWin11 <1270071138@qq.com>
2025-08-09 20:33:15 +0800

    train

--- realtime_train.py
+++ realtime_train.py
@@ -128,10 +128,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Real-time training with self-play MCTS data')
     parser.add_argument('--board-size', type=int, default=15)
     parser.add_argument('--num-workers', type=int, default=20)          # 充分利用 20 核 CPU
-    parser.add_argument('--num-simulations', type=int, default=400)      # MCTS 深度更大
+    parser.add_argument('--num-simulations', type=int, default=200)      # MCTS 深度更大
     parser.add_argument('--train-steps', type=int, default=6000)         # 100 * 20 * 3
     parser.add_argument('--batch-size', type=int, default=256)           # 3090 显存足够
-    parser.add_argument('--save_interval', type=int, default=500)
+    parser.add_argument('--save_interval', type=int, default=100)
     parser.add_argument('--queue-size', type=int, default=4096)          # 较大的自对弈数据缓存
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--log-dir', type=str, default='/root/tf-logs/')
